fbb56f5b4944ecbb23c4c2e6a0147f2d6905283c
Adam Pogwizd <adampog77@gmail.com>
2018-04-08 20:49:10 -0500

    parses weather info, need to pretty print

--- lib/weather.ex
+++ lib/weather.ex
@@ -1,18 +1,47 @@
 defmodule Weather do
-  @moduledoc """
-  Documentation for Weather.
-  """
+  require Logger
+  import SweetXml
 
-  @doc """
-  Hello world.
+  @w1_url Application.get_env(:weather, :w1_url)
 
-  ## Examples
+  # @moduledoc """
+  # Documentation for Weather.
+  # """
+  #
+  # @doc """
+  # Hello world.
+  #
+  # ## Examples
+  #
+  #     iex> Weather.hello
+  #     :world
+  #
+  # """
+  # def hello do
+  #   :world
+  # end
 
-      iex> Weather.hello
-      :world
+  def details do
+    HTTPoison.get(@w1_url)
+    |> handle_response
+    |> print_response
+  end
 
-  """
-  def hello do
-    :world
+  def handle_response({:ok, %{status_code: 200, body: body}}) do
+    body
+  end
+
+  def handle_response({_, %{status_code: status, body: body}}) do
+    Logger.error "[w1] status: #{status}, response: #{body}"
+    :error
+  end
+
+  def print_response(xml_string) do
+    IO.puts "weather: #{xpath(xml_string, ~x"//weather/text()")}"
+    IO.puts "temp: #{xpath(xml_string, ~x"//temp_f/text()")}"
+    IO.puts "wind: #{xpath(xml_string, ~x"//wind_string/text()")}"
+    IO.puts "wind chill: #{xpath(xml_string, ~x"//wind_degrees/text()")}"
+    IO.puts "relative humidity: #{xpath(xml_string, ~x"//relative_humidity/text()")}"
+    IO.puts "updated at: #{xpath(xml_string, ~x"//observation_time/text()")}"
   end
 end
